6dd639691accd3e5fb2ac033ccd499d9f1d3b3da
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-07-20 10:44:26 +0300

    Add client routes for listing and viewing transactions

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,46 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 // Basic auth routes for TechMada RH
-$routes->get('/', 'AuthController::showLogin');
-$routes->get('/login', 'AuthController::showLogin');
-$routes->post('/login', 'AuthController::login');
-$routes->post('/logout', 'AuthController::logout');
-
-// Espace personnel employe
-$routes->get('/employe/dashboard', 'EmployeController::dashboard');
-$routes->get('/employe/demandes/nouvelle', 'EmployeController::createDemande');
-$routes->get('/employe/demandes', 'EmployeController::demandes');
-$routes->post('/employe/demandes', 'EmployeController::storeDemande');
-$routes->get('/employe/profil', 'EmployeController::profile');
-$routes->post('/employe/profil', 'EmployeController::updateProfile');
-$routes->post('/employe/demandes/(:num)/annuler', 'EmployeController::cancelDemande/$1');
-
-// Dashboards RH/Admin
-$routes->get('/admin/dashboard', 'AdminController::dashboard');
-$routes->get('/admin/employes', 'AdminController::employes');
-$routes->post('/admin/employes/(:num)', 'AdminController::updateEmploye/$1');
-$routes->post('/admin/employes/(:num)/soldes/initialiser', 'AdminController::initialiseSoldes/$1');
-$routes->post('/admin/employes/(:num)/soldes', 'AdminController::saveSoldes/$1');
-$routes->get('/admin/demandes', 'AdminController::demandes');
-// Espace RH
-$routes->get('/rh/dashboard', 'RhController::dashboard');
-$routes->get('/rh/demandes', 'RhController::demandes');
-$routes->post('/rh/demandes/(:num)/approuver', 'RhController::approuver/$1');
-$routes->post('/rh/demandes/(:num)/refuser', 'RhController::refuser/$1');
-$routes->get('/rh/soldes', 'RhController::soldes');
-
-
-$routes->group('admin', ['filter' => 'role:admin'], function($routes) {
-    $routes->get('dashboard', 'AdminController::index');
-    $routes->get('users', 'AdminController::listUsers');
-    $routes->get('delete-user/(:num)', 'AdminController::delete/$1');
-});
-
-$routes->group('admin', ['filter' => 'role:admin'], function($routes) {
-    $routes->get('dashboard', 'AdminController::index');
-    $routes->get('users', 'AdminController::listUsers');
-    $routes->get('delete-user/(:num)', 'AdminController::delete/$1');
-});
-
-// Dashboards Admin
-$routes->get('/admin/dashboard', 'Home::admin');
+$routes->get('/admin/clients', 'ClientController::listeClient');
+$routes->get('/admin/clients/transactions/(:num)', 'ClientController::voirTransactionsClient/$1');
